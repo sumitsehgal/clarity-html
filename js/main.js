dae414eee91df01997a43aff55429ad6b41956fb
sumitsehgal <theprofessional1992@gmail.com>
2019-07-09 08:49:23 +0530

    Add Resources

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 4,
+        dots: false,
+        margin: 15,
+        nav: true,
+        navText: ["<img class='prev' src='images/blue-arrow.png'>", "<img class='next' src='images/blue-arrow.png'>"]
+    });
+});
