188f349c046a2f24766517d7383c4e2ddcc2c4fc
Bhogal <bhogal@bhogal-Lenovo-ideapad-310-15ISK>
2019-07-14 01:50:30 +0530

    Update about.html, css.css, and 5 more files...

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+var prevScrollpos = $(window).scrollTop();
+
 $(function() {
+    navScroll();
+    scrollId();
     $(".owl-carousel").owlCarousel({
         items: 4,
         dots: false,
@@ -7,3 +11,37 @@ $(function() {
         navText: ["<img class='prev' src='images/blue-arrow.png'>", "<img class='next' src='images/blue-arrow.png'>"]
     });
 });
+
+$(window).scroll(function() {
+    navScroll();
+});
+
+function scrollId() {
+    $(".cta a").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#get-touch").offset().top
+        }, 800);
+    });
+}
+
+function navScroll() {
+    var currentScrollPos = $(window).scrollTop(),
+        eleHead = $('#navbar');
+    console.log('pagePosition previous - ' + prevScrollpos);
+    console.log('pagePosition now - ' + currentScrollPos);
+    if (currentScrollPos > eleHead.height()) {
+        eleHead.find('.top').hide();
+        eleHead.addClass('fix');
+        $('body').css('padding-top', eleHead.height());
+        if (eleHead.hasClass('fix') && prevScrollpos > currentScrollPos) {
+            eleHead.css('top', '0');
+        } else {
+            eleHead.css('top', '-90px');
+        }
+        prevScrollpos = currentScrollPos;
+    } else {
+        eleHead.find('.top').removeAttr('style');
+        eleHead.removeClass('fix');
+        $('body').removeAttr('style');
+    }
+}
